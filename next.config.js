d155c2ab7a12a1591d9cf33a04edf14696112da0
Luis Torres <luis.torres@cl.gt.com>
2023-03-27 02:16:53 -0300

    Add headers

--- next.config.js
+++ next.config.js
@@ -2,6 +2,23 @@
 const nextConfig = {
     reactStrictMode: true,
     pageExtensions: ['html', 'jsx', 'js', 'tsx', 'ts'],
+    headers: async () => {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Cross-Origin-Opener-Policy',
+                        value: 'same-origin',
+                    },
+                    {
+                        key: 'Cross-Origin-Embedder-Policy',
+                        value: 'require-corp',
+                    },
+                ],
+            },
+        ]
+    }
 }
 
 module.exports = nextConfig
