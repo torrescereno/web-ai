8237e9ef3ab9799ada1cc527dc3b6fc4824fad16
Luis Torres <luis.torres@cl.gt.com>
2023-03-27 02:02:04 -0300

    Change page extensions

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js']
+    pageExtensions: ['html', 'jsx', 'js', 'tsx', 'ts'],
 }
 
 module.exports = nextConfig
