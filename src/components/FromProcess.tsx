ede5706d8ed6efc8509d33686692d05999d8419a
Luis Torres <luis.torres@cl.gt.com>
2023-03-27 11:55:19 -0300

    Fix bg process

--- src/components/FromProcess.tsx
+++ src/components/FromProcess.tsx
@@ -162,7 +162,7 @@ export const FromProcess = () => {
                     )
                     : (<>
                         <div
-                            className="block w-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+                            className="block w-full p-6 border rounded-lg shadow bg-gray-800 border-gray-700"
                         >
                             {completions}
                         </div>
