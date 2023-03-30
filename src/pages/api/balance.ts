a489d8a6d3dfe2bf6e94ad2e13d8a2de5245f83b
Luis Torres <luis.torres@cl.gt.com>
2023-03-30 00:03:16 -0300

    Add load env

--- src/pages/api/balance.ts
+++ src/pages/api/balance.ts
@@ -4,6 +4,10 @@ import {HNSWLib} from "langchain/vectorstores";
 import {OpenAIEmbeddings} from "langchain/embeddings";
 import {RetrievalQAChain} from "langchain/chains";
 
+import * as dotenv from "dotenv";
+
+dotenv.config()
+
 export default async function handler(req: any, res: any) {
 
     const {prompt, apiKey, text} = JSON.parse(req.body)
