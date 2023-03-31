b9e1ad9c4f42c765fc03fc30b2a149cbb989f41e
Luis Torres <luis.torres@cl.gt.com>
2023-03-30 23:40:04 -0300

    Add prompt templates

--- src/pages/api/balance.ts
+++ src/pages/api/balance.ts
@@ -1,8 +1,9 @@
-import {OpenAI} from "langchain/llms";
+import {OpenAIChat} from "langchain/llms";
 import {RecursiveCharacterTextSplitter} from "langchain/text_splitter";
 import {HNSWLib} from "langchain/vectorstores";
 import {OpenAIEmbeddings} from "langchain/embeddings";
-import {RetrievalQAChain} from "langchain/chains";
+import {LLMChain, ChatVectorDBQAChain, loadQAChain} from "langchain/chains";
+import {PromptTemplate} from 'langchain/prompts';
 
 import * as dotenv from "dotenv";
 
@@ -11,16 +12,51 @@ dotenv.config()
 export default async function handler(req: any, res: any) {
 
     const {prompt, apiKey, text, temperature} = JSON.parse(req.body)
-    const model = new OpenAI({temperature: +temperature, openAIApiKey: apiKey});
+
+    // --
 
     const textSplitter = new RecursiveCharacterTextSplitter({chunkSize: 1000});
     const docs = await textSplitter.createDocuments([text]);
     const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
-    const chain = RetrievalQAChain.fromLLM(
-        model, vectorStore.asRetriever());
+
+    // --
+
+    const CONDENSE_PROMPT =
+        PromptTemplate.fromTemplate(`Eres un contador auditor experto`);
+
+    const QA_PROMPT = PromptTemplate.fromTemplate(
+        `Eres una asistente que analizará el texto como un contador auditor experto, no inventes montos ni datos, solo responde a las preguntas en base al texto y al contexto
+                Pregunta: {question}
+                =========
+                {context}
+                =========
+                Response en Texto:`,
+    );
+
+    const questionGenerator = new LLMChain({
+        llm: new OpenAIChat({temperature: temperature}),
+        prompt: CONDENSE_PROMPT,
+    });
+
+    const docChain = loadQAChain(
+        new OpenAIChat({
+            openAIApiKey: apiKey,
+            temperature: 0,
+            modelName: 'gpt-3.5-turbo',
+        }),
+        {prompt: QA_PROMPT},
+    );
+
+    const chain = new ChatVectorDBQAChain({
+        vectorstore: vectorStore,
+        combineDocumentsChain: docChain,
+        questionGeneratorChain: questionGenerator,
+        returnSourceDocuments: true
+    });
+
     const response = await chain.call({
-        input_documents: docs,
-        query: prompt
+        question: prompt,
+        chat_history: [],
     });
 
     res.status(200).json({response: response.text})
